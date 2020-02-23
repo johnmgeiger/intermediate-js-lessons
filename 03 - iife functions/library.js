3e4c9c1c1c9ad9e17013a3336bc755779518b606
Johnathan Geiger <johnmgeiger@gmail.com>
2020-02-22 20:18:53 -0600

    lesson 3 completed

--- 03 - iife functions/library.js
+++ 03 - iife functions/library.js
@@ -4,15 +4,31 @@
 //      - track how many times a color is changed
 //      - make that number of changes accessible with a getter
 
-const hoverElements = document.getElementsByClassName('hover-me')
+const hoverMe = (function () {
+    let colorChangerCounter = 0
 
-for (el of hoverElements) {
-    const originalColor = el.style.color
-    
-    el.addEventListener('mouseover', ev => {
-        ev.target.style.color = "#888"
-    })
-    el.addEventListener('mouseout', ev => {
-        ev.target.style.color = originalColor
-    })
-}
+    const incramentColorCounter = () => {
+        colorChangerCounter += 1
+    }
+
+    const hoverElements = document.getElementsByClassName('hover-me')
+
+    for (el of hoverElements) {
+        const originalColor = el.style.color
+
+        el.addEventListener('mouseover', ev => {
+            incramentColorCounter()
+            ev.target.style.color = "#888"
+        })
+        el.addEventListener('mouseout', ev => {
+            incramentColorCounter()
+            ev.target.style.color = originalColor
+        })
+    }
+
+    const colorChanges = () => colorChangerCounter
+
+    return {
+        colorChanges,
+    }
+}())
